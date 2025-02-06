6118f64a5b8a6a756385ab3d353c91cca6614b9a
Kumar-Munna-dev <fullstacks.dev@gmail.com>
2025-02-06 07:11:36 -0600

    2nd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2711762220"
+    serviceWorkerVersion: "1329483112"
   }
 });
